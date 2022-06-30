aaf6eba80b6ad84c19921acaef98afe99c14c0ba
Leegukhyoun <dbkim512@naver.com>
2022-06-30 16:58:46 +0900

    mashup-todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './component/TodoTemplate';
+import TodoHead from './component/TodoHead';
+import TodoList from './component/TodoList';
+import TodoCreate from './component/TodoCreate';
+import { TodoProvider } from './TodoContext';
+
+//글로벌 스타일을 추가하고싶은 때
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoProvider>
+      <GlobalStyle/>
+      <TodoTemplate>
+        <TodoHead></TodoHead>
+        <TodoList></TodoList>
+        <TodoCreate></TodoCreate>
+      </TodoTemplate>
+    </TodoProvider>
   );
 }
 
